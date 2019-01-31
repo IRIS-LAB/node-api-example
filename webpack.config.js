a2137deec89d40191c5bf57e24834ff73ff9fcbe
Benjamin Corré <benjamin.corre@systeme-u.fr>
2019-01-31 09:06:08 +0100

    feat(environnement): add variables environments

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,32 @@
 var path = require('path')
+var Dotenv = require('dotenv-webpack')
 var nodeExternals = require('webpack-node-externals')
 
 module.exports = {
-	entry: './server.js',
-	target: 'node', // in order to ignore built-in modules like path, fs, etc.
-	externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
-	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: 'server.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env'],
-						plugins: ['@babel/plugin-transform-runtime']
-					}
-				}
-			}
-		]
-	}
+  entry: './server.js',
+  target: 'node', // in order to ignore built-in modules like path, fs, etc.
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'server.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new Dotenv({
+      path: './.env.production'
+    })
+  ]
 }
